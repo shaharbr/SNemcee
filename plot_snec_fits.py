a5569b5ce9893ae221f3fb5ed281ce34a0ae977c
Shahar <shaharbra@gmail.com>
2022-09-11 12:28:25 -0400

    fix bugs

--- plot_snec_fits.py
+++ plot_snec_fits.py
@@ -190,6 +190,7 @@ def plot_mag_with_fit(data_dict, sampler_df, ranges_dict, n_walkers, ax, normali
                     log_likeli.append(mcmc_snec.calc_likelihood(data_x_filt_moved, data_y_filt, data_dy_filt,
                                                                 y_fit_on_data_times[filt], normalization))
         else:
+            print(requested, ranges_dict)
             print('not in range')
     log_likeli = np.mean(log_likeli)
     for filt in filters:
